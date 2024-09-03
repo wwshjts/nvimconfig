11e0ecbf9ea422e71c70ee95c9797a856c2ee770
Yury Kareba <wwsh@gmail.com>
2024-09-04 01:17:07 +0700

    Changed note id generation algorithm

--- lua/base/config/nvim-tree.lua
+++ lua/base/config/nvim-tree.lua
@@ -4,6 +4,10 @@ require('nvim-tree').setup({
     view = {
         width = 25,
     },
+    filters = {
+        -- hide dotfiles
+        dotfiles = true,
+    },
 })
 
 nm('<leader>e', ':NvimTreeToggle <cr>')
--- lua/base/config/obs.lua
+++ lua/base/config/obs.lua
@@ -4,7 +4,24 @@ require('obsidian').setup({
           name = "math",
           path = "~/vaults/math",
         },
-    }
+    },
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+        local correct_name = ""
+        if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            -- correct_name = title:gsub(" ", "-"):gsub("[^A-Za-z0-9]", ""):lower()
+            correct_name = title:gsub(" ", "_"):gsub("[^A-Za-z0-9A-Яа-я]", ""):lower()
+        else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            for _ = 1, 4 do
+                correct_name = correct_name .. string.char(math.random(65, 90))
+            end
+        end
+        return correct_name
+    end,
 })
 
 -- need for some obsidian features
